6f43a7e3c4892cd496986ca7c0cc1ba4722e2bdb
Banks Nussman <banks@nussman.us>
2022-04-09 13:21:45 -0400

    kind of works with state

--- app/App.tsx
+++ app/App.tsx
@@ -3,16 +3,6 @@ import React, {useState} from 'react';
 import { Audio } from 'expo-av';
 
 export default function App() {
-    const [audio, setAudio] = useState([
-        { name: 'audio1', key: '1' },
-        { name: 'audio2', key: '2' },
-        { name: 'audio3', key: '3' },
-        { name: 'audio4', key: '4' },
-        { name: 'audio5', key: '5' },
-        { name: 'audio6', key: '6' },
-    ]);
-
-
     const [recording, setRecording] = React.useState();
     const [recordings, setRecordings] = React.useState([]);
     const [message, setMessage] = React.useState("");
@@ -56,30 +46,37 @@ export default function App() {
         setRecordings(updatedRecordings);
     }
 
+    async function playAudio(uri: string) {
+        console.log('Loading Sound');
+        const { sound } = await Audio.Sound.createAsync({ uri });
+        console.log('Playing Sound');
+        await sound.playAsync();
+    }
+
     return (
         <NativeBaseProvider>
-        <Center bg='hsl(0, 0%, 97%)' h='100%'>
-        <Center h='70%'>
-        <Center mt="-5%" shadow="4" rounded={25} bg='red.400' alignItems="center" justifyContent="center" w={50} h={50} mb="5%">
-        <Text bold color='white' fontSize={25} fontFamily="AppleSDGothicNeo-Bold">&#127798;</Text>
-        </Center>
-        <Center mb='15%'>
-        <FlatList
-        data={recordings}
-        renderItem={({ item }) => (
-            <VStack space={recordings.length-1} alignItems="center">
-            <Pressable onPress={() => playbackObject.replayAsync(0)} title="Play" w="64" h="60" bg="white" rounded={15} mt={3} mb={3} shadow="1" alignItems="center" justifyContent="center">
-            <Text>{item.file}</Text>    
-            </Pressable>
-            </VStack>
-        )}
-        />
-        </Center>
-        <Pressable bg='red.400' w='100px' h='100px' rounded={50} mt='-50px' alignItems='center' justifyContent='center' shadow="4" onPressIn={startRecording} onPressOut={stopRecording}>
-        <Text>Record</Text>
-        </Pressable>
-        </Center>
-        </Center>
+            <Center bg='hsl(0, 0%, 97%)' h='100%'>
+                <Center h='70%'>
+                    <Center mt="-5%" shadow="4" rounded={25} bg='red.400' alignItems="center" justifyContent="center" w={50} h={50} mb="5%">
+                        <Text bold color='white' fontSize={25} fontFamily="AppleSDGothicNeo-Bold">&#127798;</Text>
+                    </Center>
+                    <Center mb='15%'>
+                        <FlatList
+                            data={recordings}
+                            renderItem={({ item }) => (
+                                <VStack space={recordings.length-1} alignItems="center">
+                                    <Pressable onPress={() => playAudio(item.file)} title="Play" w="64" h="60" bg="white" rounded={15} mt={3} mb={3} shadow="1" alignItems="center" justifyContent="center">
+                                        <Text>{item.file}</Text>    
+                                    </Pressable>
+                                </VStack>
+                            )}
+                        />
+                    </Center>
+                    <Pressable bg='red.400' w='100px' h='100px' rounded={50} mt='-50px' alignItems='center' justifyContent='center' shadow="4" onPressIn={startRecording} onPressOut={stopRecording}>
+                        <Text>Record</Text>
+                    </Pressable>
+                </Center>
+            </Center>
         </NativeBaseProvider>
     );
 }
