0822a6d5498b197987cf1f2f79ae411a694b1306
Banks Nussman <banks@nussman.us>
2022-04-08 21:26:09 -0400

    clean up App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,28 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
+import { Text, NativeBaseProvider } from 'native-base';
+import React from 'react';
 
 export default function App() {
     return (
         <NativeBaseProvider>
-            <View style={styles.container}>
-                <Text style={styles.logo}>Pepper</Text>
-                <StatusBar style="auto" />
-            </View>
+            <Text bold color="green">testing dggewqjgjdk</Text>
         </NativeBaseProvider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'hsl(0, 0%, 98%)',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logo: {
-        fontSize: 18,
-        fontWeight: 'bold',
-
-    },
-});
